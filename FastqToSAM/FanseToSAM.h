a5b1c371821ba1fca23cdb5c50cb4356b658772b
郑廷锴 <1129664307@qq.com>
2017-05-27 16:34:08 +0800

    本程序用于将Fanse3g基因组mapping后所得的.fanse文件转化为SAM格式文件.sam 参数只有一个，即要转换的fanse文件的文件名，即： FanseToSAM.exe ~path/xxx.fanse

---
+++ FastqToSAM/FanseToSAM.h
@@ -0,0 +1,8 @@
+#pragma once
+#include<iostream>
+#include<string>
+using std::string;
+
+struct swiff {
+	string dataname;
+};
---
+++ FastqToSAM/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<fstream>
+#include"FanseToSAM.h"
+using std::cout;
+using std::cin;
+int main(int argc,char*argv[]) {
+	if (argc > 2) {
+		cout << "输入参数过多，程序退出。";
+	}
+}
