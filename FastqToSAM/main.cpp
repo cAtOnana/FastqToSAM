1cf75960986ecf93e4d620c777e623820051271f
郑廷锴 <1129664307@qq.com>
2017-05-30 13:39:05 +0800

    coding

--- FastqToSAM/main.cpp
+++ FastqToSAM/main.cpp
@@ -2,17 +2,22 @@
 #include<fstream>
 #include<ctime>
 #include"FanseToSAM.h"
+#include<vector>
 using std::cout;
 using std::cin;
 using std::ifstream;
 using std::ofstream;
 using std::endl;
+using std::vector;
 int main(int argc,char*argv[]) {
 	string log = string(argv[1]);
 	log = log.erase(log.length() - 6) + "log";
 	ofstream outlog(log);
-
-	if (argc > 2) {
+	if (!outlog.is_open()) {
+		cout << "log文件打开失败，程序退出。";
+		exit(EXIT_FAILURE);
+	}
+	if (argc > 3) {
 		cout << "输入参数过多，程序退出。";
 		outlog << argv[1] << "处理失败，原因：输入参数过多。" << endl;
 		exit(EXIT_FAILURE);
@@ -23,5 +28,5 @@ int main(int argc,char*argv[]) {
 		outlog << argv[1] << "处理失败，原因：fanse文件打开失败，请查看输出路径以确认fanse文件在路径中。" << endl;
 		exit(EXIT_FAILURE);
 	}
-	
+	vector<fanse> list;
 }
